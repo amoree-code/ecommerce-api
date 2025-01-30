2b600f3eee29ea309b64ada779fef30657d658d0
amoree <amoreevw@gmail.com>
2025-01-30 18:16:35 +0300

    update

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,7 @@ router.post("/register", async (req, res) => {
         process.env.PASS_SEC
       ).toString(),
     });
+
     const sevedUser = await user.save();
     res.status(201).json(sevedUser);
   } catch (err) {
